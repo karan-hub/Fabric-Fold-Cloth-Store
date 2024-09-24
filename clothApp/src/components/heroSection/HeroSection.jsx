c7cd0edcc284d22f4d08cdc203797e66fa30f688
karan <karanc4143@gmail.com>
2024-09-24 21:00:19 +0530

    h

--- clothApp/src/components/heroSection/HeroSection.jsx
+++ clothApp/src/components/heroSection/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Link } from 'react-router-dom';
 
 gsap.registerPlugin(ScrollTrigger);
 
